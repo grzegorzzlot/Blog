e380cc421bcd0a9e617c352e4bc9e3187046ab8b
Grzegorz Zlot <gzlot@onet.eu>
2018-10-21 19:31:34 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+app.use(morgan('combined'));
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.listen(3000, ()=>{
+  console.log(`listening on port ${chalk.green('3000')}`);
+})
